e80014df0e7961e470257838508cc6f4fb4baa18
FrankyATeen <tankthegamer.nl@gmail.com>
2014-11-19 19:21:29 -0600

    Added the ToDo text file and add a play meathod that loops

--- TextRpg/src/FranklyATeen/TextRpg/TextRpg.java
+++ TextRpg/src/FranklyATeen/TextRpg/TextRpg.java
@@ -2,8 +2,25 @@ package FranklyATeen.TextRpg;
 
 public class TextRpg {
 
+	
+	private static boolean playing;
+	
 	public static void main(String[] args) {
-		
+		play();
+	}
+
+	private static void play() {
+		// TODO Auto-generated method stub
+		playing = true;
+		while(playing){
+			
+			
+			
+			
+			// this is so cpu does not get overloaded
+			try {	Thread.sleep(50); } catch (InterruptedException e) {/* TODO Auto-generated catch block*/ e.printStackTrace();
+			}
+		}
 	}
 
 }
